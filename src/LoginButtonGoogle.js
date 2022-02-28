45a2159a63da6e360754144161b6557d3d266ef9
tr0ub1eM4k3r <61628220+tr0ub1eM4k3r@users.noreply.github.com>
2022-02-28 16:06:52 +0100

    fixed login and added logout

--- src/LoginButtonGoogle.js
+++ src/LoginButtonGoogle.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import GoogleLogin from 'react-google-login'
 import { FlowRight } from './Styles/Styled'
+import { GoogleLogout } from 'react-google-login'
 
 
 export default function LoginButtonGoogle() {
@@ -8,17 +9,28 @@ export default function LoginButtonGoogle() {
     console.log(response);
   }
 
+  const logout = (response) => {
+    console.log(response);
+  }
+
+
   return (
     <Fragment>
       <FlowRight>
         <GoogleLogin
           clientId="258375953305-7j1t10b7nlkrlgclfm94a86q55k3v5d9.apps.googleusercontent.com"
-          buttonText="Login"
+          buttonText="Login with Google"
           onSuccess={responseGoogle}
           onFailure={responseGoogle}
           cookiePolicy={'single_host_origin'}
           isSignedIn={true}
         />
+
+        <GoogleLogout
+          clientId="258375953305-7j1t10b7nlkrlgclfm94a86q55k3v5d9.apps.googleusercontent.com"
+          buttonText="Logout"
+          onLogoutSuccess={logout}>
+        </GoogleLogout>
       </FlowRight>
     </Fragment>
   )
