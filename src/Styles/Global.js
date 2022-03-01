5235795ae8ceceea80ec21e91cfb547051fdef2b
tr0ub1eM4k3r <61628220+tr0ub1eM4k3r@users.noreply.github.com>
2022-03-01 12:31:24 +0100

    bigIncrease to Global styles, navbar performance

--- src/Styles/Global.js
+++ src/Styles/Global.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
-const GlobalStyles = createGlobalStyle`
+export const GlobalStyles = createGlobalStyle`
     body {
         background-image: url(../images/workflowGuy.jpg);
         background-position: center;
@@ -8,7 +8,17 @@ const GlobalStyles = createGlobalStyle`
         background-size: cover;
         overflow: hidden;
         background-position: center;
+        margin: 0;
+        padding: 0;
     }
 `
 
-export default GlobalStyles
+export const GlobalBackground = createGlobalStyle`
+    body {
+        background: linear-gradient(90deg ,hsla(250, 100%, 32%, 1), hsl(210, 84.8%, 51%));
+        margin: 0;
+        padding: 0;
+        background-size: cover;
+    }
+
+`
