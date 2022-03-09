d5d9927c479b8ee3c5c8b15f3ff73cf8e2372bbf
tr0ub1eM4k3r <61628220+tr0ub1eM4k3r@users.noreply.github.com>
2022-03-09 12:10:04 +0100

    fixed login button (#18)

--- src/components/univeralComponents/NavbarComponents/NavLoginButton.js
+++ src/components/univeralComponents/NavbarComponents/NavLoginButton.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { LoginButton } from '../../../Styles/Styled'
+import React from "react";
+import { Link } from "react-router-dom";
+import { LoginButton } from "../../../Styles/Styled";
+import styled from "styled-components";
 
+const StyledLink = styled(Link)`
+  margin-left: auto;
+`;
 export default function NavLoginButton() {
   return (
-    <Link to="/login">
-        <LoginButton type="button">Login</LoginButton>
-    </Link>
-  )
+    <StyledLink to="/login">
+      <LoginButton type="button">Login</LoginButton>
+    </StyledLink>
+  );
 }
