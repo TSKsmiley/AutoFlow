547aa28f182fef029aab187c65bf1c7f8b3f2597
AlbertHald <albert24122@gmail.com>
2022-03-09 15:10:33 +0100

    Made it not landing

--- src/components/univeralComponents/NavbarComponents/Navbutton.js
+++ src/components/univeralComponents/NavbarComponents/Navbutton.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useContext } from "react";
 import { Link } from 'react-router-dom'
 import { NavButton } from '../../../Styles/Styled'
+import { Context } from '../../../Context'
+
 
 export default function Navbutton() {
+  
+  const { isLoggedIn } = useContext(Context);
+  
+  function returnLinkTo() {
+     if (isLoggedIn) {
+      return "/panel";
+    }
+    return "/";
+  }
+
   return (
-    <Link to="/">
+    <Link to= {returnLinkTo}>
         <NavButton type="button">AutoFlow</NavButton>
     </Link>
   )
