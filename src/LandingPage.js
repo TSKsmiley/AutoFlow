b57967bf6b7ad1a86d1f9ae0da05453d38e6bf87
BaBrixx <90306530+BaBrixx@users.noreply.github.com>
2022-03-09 16:12:53 +0100

    fixed /panel accessibility (#21)

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,11 +1,22 @@
-import { React, useContext, useEffect } from "react";
+import { React, useContext, useEffect, useState } from "react";
 import { Context } from "./Context";
 import { GlobalStyles } from "./Styles/Global";
 import Navbar from "./components/univeralComponents/Navbar";
 import MainScreen from "./components/landingPage/MainScreen";
+import { useNavigate } from "react-router-dom";
 
 export default function LandingPage() {
-  const { showPanelNav, setShowPanelNav } = useContext(Context);
+  const { showPanelNav, setShowPanelNav, isLoggedIn } = useContext(Context);
+  const [__isMounted, setMounted] = useState(true);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(isLoggedIn && __isMounted) {
+        navigate("/panel");
+      }
+    return () => setMounted(false);
+  }, []);
 
   useEffect(() => {
     setShowPanelNav(false);
--- src/MainPanel.js
+++ src/MainPanel.js
@@ -1,10 +1,22 @@
-import { React, useContext, useEffect } from "react";
+import { React, useContext, useEffect, useState } from "react";
 import Navbar from "./components/univeralComponents/Navbar";
 import { GlobalBackground } from "./Styles/Global";
 import { Context } from "./Context";
+import { useNavigate } from "react-router-dom";
 
 export default function MainPanel() {
-  const { showPanelNav, setShowPanelNav } = useContext(Context);
+  const { showPanelNav, setShowPanelNav, isLoggedIn } = useContext(Context);
+  const [__isMounted, setMounted] = useState(true);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(!isLoggedIn && __isMounted) {
+        navigate("/");
+    }
+    
+    return () => setMounted(false);
+  }, []);
 
   useEffect(() => {
     setShowPanelNav(true);
