251faba21d4d0f2ca3201fbb721bf69208614d8f
tr0ub1eM4k3r <61628220+tr0ub1eM4k3r@users.noreply.github.com>
2022-03-16 15:36:08 +0100

    navbarpeneler api comm (#32)
    * navbarpeneler api comm
    
    * fixed add button cause ugly

--- src/components/webPanel/FlowComponent.js
+++ src/components/webPanel/FlowComponent.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { Box } from '../../Styles/Styled'
 import ToggleableButton from '../univeralComponents/ToggleableButton'
 
-export default function FlowComponent() {
+export default function FlowComponent( from, to, toggled = false ) {
   return (
     <div>
         <Box>
-            <ToggleableButton />
+            <ToggleableButton toggled={toggled}/>
         </Box>
     </div>
   )
--- src/components/webPanel/FlowsPanel.js
+++ src/components/webPanel/FlowsPanel.js
@@ -1,20 +1,49 @@
-import React, { useState } from 'react'
-import { MainPanelGrid } from '../../Styles/Styled'
+import React, { useEffect, useState } from 'react'
+import { AddButton, MainPanelGrid } from '../../Styles/Styled'
 import FlowComponent from './FlowComponent'
 import { Box } from '../../Styles/Styled';
 
 export default function FlowsPanel() {
-    const [cardsAmount, updateCardsAmount] = useState(0);
+    const [flows, setFlows] = useState(null)
+    const api_url = "https://devapi.aau-sw.dk/flows/" // maybe not right url
+    
+    const defaultFlows = {
+      flows: []
+    }
 
+    useEffect(() => {
+      setFlows(defaultFlows.flows)
+      return
+      let data = fetch(api_url)
+      .then(result => {
+        result.json()
+      })
+      setFlows(data.flows)
+      return
+      
+    }, [])
+
+
+
+    function handleOnClick(){
+      console.log(flows)
+      setFlows([...flows, {}])
+    }
+    
+    
+    
+    
+  console.log(defaultFlows.flows.length)
 
   return (
     <MainPanelGrid>
-        {[...Array(cardsAmount)].map((i) => <FlowComponent key={i}/>)}
+      {console.log(flows)}
+        { flows?.map((element, i) => (
+          <FlowComponent from={element.from} to={element.to} key={i}/>
+        ))}
         
-        <button type='button' onClick={() => updateCardsAmount(prevCount => prevCount + 1)}>
-          <Box>
-          </Box>
-        </button>
+        <AddButton type='button' onClick={handleOnClick}>
+        </AddButton>
       
 
     </MainPanelGrid>
